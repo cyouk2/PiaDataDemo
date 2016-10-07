2f91404533125e1d29ac1bb48a7f6ac92549653a
cyouk2 <cyouk2@OH1144.jpn.ithd.local>
2016-10-07 15:28:08 +0900

    test

---
+++ src/com/zgd/Serve.java
@@ -0,0 +1,48 @@
+package com.zgd;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.google.appengine.api.blobstore.BlobKey;
+import com.google.appengine.api.blobstore.BlobstoreInputStream;
+import com.google.appengine.api.datastore.DatastoreService;
+import com.google.appengine.api.datastore.DatastoreServiceFactory;
+import com.google.appengine.api.datastore.Entity;
+
+@SuppressWarnings("serial")
+public class Serve extends HttpServlet {
+
+	@Override
+	public void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException {
+
+		String keyString = req.getParameter("blob-key");
+		BlobKey blobkey = new BlobKey(keyString);
+		BufferedReader reader = new BufferedReader(new InputStreamReader(new BlobstoreInputStream(blobkey)));
+		String line = null;
+		
+		
+		String columns = "playDate,taiNo,bonusCount,rate,ballOutput,ballInput,totalOut,totalOutBefore,rateN,bonusCountN,ballOutputN,playDateN,totalOutN";
+		String[] a = columns.split(",");
+		while ((line = reader.readLine()) != null) {
+			// Do whatever you like with the line
+			savepidata(line.split(","), a);
+		}
+		reader.close();
+	}
+	
+	private void savepidata(String[] line,String[] a){
+		
+		DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+		Entity employee = new Entity("PIA_DATA_INFO");
+		for(int i = 0; i < a.length; i++){
+			employee.setProperty(a[i], line[i]);
+		}
+		
+		datastore.put(employee);
+	}
+}
---
+++ war/index.jsp
@@ -0,0 +1,24 @@
+<%@ page language="java" contentType="text/html; charset=utf-8"
+	pageEncoding="utf-8"%>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<%@ page
+	import="com.google.appengine.api.blobstore.BlobstoreServiceFactory"%>
+<%@ page import="com.google.appengine.api.blobstore.BlobstoreService"%>
+
+<%
+	BlobstoreService blobstoreService = BlobstoreServiceFactory.getBlobstoreService();
+%>
+
+
+<html>
+<head>
+<title>Upload Test</title>
+</head>
+<body>
+	<form action="<%=blobstoreService.createUploadUrl("/upload")%>"
+		method="post" enctype="multipart/form-data">
+		<input type="text" name="foo"> <input type="file"
+			name="myFile"> <input type="submit" value="Submit">
+	</form>
+</body>
+</html>
